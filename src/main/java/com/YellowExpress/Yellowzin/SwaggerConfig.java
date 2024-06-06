f694c3329ed84a361a6d0c41371daea30c8eb912
Davi Andrzejewski Junkes <108496905+JnksDavu@users.noreply.github.com>
2024-06-06 18:41:54 -0300

    Adicionado lógica do Swagger

---
+++ src/main/java/com/YellowExpress/Yellowzin/SwaggerConfig.java
@@ -0,0 +1,9 @@
+package com.YellowExpress.Yellowzin;
+ 
+import io.swagger.v3.oas.models.OpenAPI;
+ 
+public class SwaggerConfig {
+    public OpenAPI customOpenAPI() {
+		return new OpenAPI();
+	}
+}
