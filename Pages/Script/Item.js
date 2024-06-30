de27ae99d166ac0ae6bee99ca7bd1552412073a2
unknown <gualquinies@hotmail.com>
2024-06-30 01:13:52 -0300

    Chamada do item para item detalhado

--- Pages/Script/Item.js
+++ Pages/Script/Item.js
@@ -1,9 +1,23 @@
-// Função para buscar os detalhes do produto por ID
-function buscarDetalhesProduto(id) {
-    // Endpoint do seu backend Spring Boot
-    const endpoint = `http://localhost:8080/api/produtos/${id}`;
+document.addEventListener('DOMContentLoaded', function() {
+    const produtoDetalhes = sessionStorage.getItem('produtoDetalhes');
+    if (produtoDetalhes) {
+        const produto = JSON.parse(produtoDetalhes);
+        const nomeProdutoElement = document.getElementById('nomeProduto');
+        const descricaoProdutoElement = document.getElementById('descricaoProduto');
+        const valorProdutoElement = document.getElementById('valorProduto');
+        const imgProdutoElement = document.getElementById('imgProduto');
 
-    // Fazendo uma requisição GET para o endpoint
+        nomeProdutoElement.textContent = produto.nomeProduto;
+        descricaoProdutoElement.textContent = produto.descricaoProduto;
+        valorProdutoElement.textContent = `R$ ${produto.valorProduto.toFixed(2)}`;
+        imgProdutoElement.src = produto.img;
+    } else {
+        console.error('Dados do produto não encontrados na sessionStorage.');
+    }
+});
+function buscarDetalhesProduto(id) {
+    const endpoint = `http://localhost:8080/api/Produtos/${id}`;
+
     fetch(endpoint)
         .then(response => {
             if (!response.ok) {
@@ -12,28 +26,21 @@ function buscarDetalhesProduto(id) {
             return response.json();
         })
         .then(data => {
-            // Manipular os dados recebidos (data) aqui
             console.log('Detalhes do Produto:', data);
 
-            // Exemplo de como você pode acessar os campos retornados
-            const nomeProduto = data.nomeProduto;
-            const descricaoProduto = data.descricaoProduto;
-            const valorProduto = data.valorProduto;
-            const img = data.img;
+            sessionStorage.setItem('produtoDetalhes', JSON.stringify(data));
 
-            // Redirecionar para a página de detalhes do produto com os dados recebidos
-            window.location.href = `produtosdetalhados.html?id=${id}`;
+            window.location.href = `./product-details.html`;
         })
         .catch(error => {
             console.error('Erro ao buscar detalhes do produto:', error);
-            // Tratar o erro de forma adequada, por exemplo, exibindo uma mensagem para o usuário
         });
 }
 
-// Exemplo de uso: chamar a função quando clicar no link do produto
 const linkProduto = document.querySelector('h4 a');
 linkProduto.addEventListener('click', function(event) {
     event.preventDefault();
     const productId = event.target.getAttribute('href').split('=')[1];
     buscarDetalhesProduto(productId);
 });
+
